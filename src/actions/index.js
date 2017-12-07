dcd4772e990ecf9cc98575e238c69da3d38e5935
conmart <connor.a.martinelli@gmail.com>
2017-12-06 21:04:24 -0800

    login built

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED 
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER
 } from './types';
+
 
 export const emailChanged = (text) => {
   return {
@@ -16,3 +21,28 @@ export const passwordChanged = (text) => {
     payload: text
   };
 };
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch))
+      });
+  };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL })
+}
